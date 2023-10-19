cdf4e2ee361fc3b4e5ec91d5976c9b9210360462
MuhammadAdnan11 <adnan191022@gmail.com>
2023-10-19 10:01:45 +0500

    updated

--- myportfolio/src/pages/Home/Home.js
+++ myportfolio/src/pages/Home/Home.js
@@ -1,11 +1,25 @@
 import React from "react";
+import { useTheme } from "../../context/ThemeContext";
+import { BsFillMoonStarsFill, BsFillSunFill } from "react-icons/bs";
 import "./home.css";
 import Typewriter from "typewriter-effect";
 import Adnan from "../../assets/docs/Adnan.pdf";
 export const Home = () => {
+  const [theme, setTheme] = useTheme();
+  //handle theme
+  const handleTheme = () => {
+    setTheme((prevState) => (prevState === "light" ? "dark" : "light"));
+  };
   return (
     <>
       <div className="container-fluid home-container" id="home">
+        <div className="theme-btn" onClick={handleTheme}>
+          {theme == "light" ? (
+            <BsFillMoonStarsFill size={23} />
+          ) : (
+            <BsFillSunFill size={23} />
+          )}
+        </div>
         <div className="container home-content">
           <h2>Hi 👋 I'm a</h2>
           <h1>
