30851ec0256197633c489762d1ff9704a465964e
MuhammadAdnan11 <adnan191022@gmail.com>
2023-10-23 12:46:05 +0500

    updated

--- client/src/pages/About/About.js
+++ client/src/pages/About/About.js
@@ -8,10 +8,7 @@ const About = () => {
         <div className="about" id="about">
           <div className="row">
             <div className="col-md-6 col-xl-6 col-lg-6 col-xs-12 about-img">
-              <img
-                src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSzHQv_th9wq3ivQ1CVk7UZRxhbPq64oQrg5Q&usqp=CAU"
-                alt="profile_pic"
-              />
+              <img src="./adnan.jpg" alt="profile_pic" />
             </div>
             <div className="col-md-6 col-xl-6 col-lg-6 col-xs-12 about-content">
               <h1>About me</h1>
